e7c691a5d3178c5f8aeea0efd1afb939b1de77da
yasin-mehdiyev <30857286+yasin-mehdiyev@users.noreply.github.com>
2020-06-08 10:32:24 +0400

    To Find Palindrom Words(With Function and With Recursive Func)

---
+++ Palindroms/Palindroms/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Palindroms
+{
+    class Program
+    {
+        //Palindroms Words with Function
+        public static bool isPalindromes(string str)
+        {
+            int initilIndex = 0;
+            int lastIndex = str.Length - 1;
+
+            while (lastIndex>initilIndex)
+            {
+                if (str[initilIndex++]!=str[lastIndex--])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //Palindroms Words with Recursive Function
+        public static bool isPalindromsWithRecursion(string str)
+        {
+            if (str.Length<=1)
+            {
+                return true;
+            }
+            else
+            {
+                if (str[0]!=str[str.Length-1])
+                {
+                    return false;
+                }
+                else
+                {
+                    return isPalindromes(str.Substring(1, str.Length - 2));
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String Word");
+            string str = Console.ReadLine();
+
+            //if (!isPalindromes(str))
+            //{
+            //    Console.WriteLine("False,It is not a palinrom word");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("True,It is a palinrom word");
+            //}
+
+            if (!isPalindromsWithRecursion(str))
+            {
+                Console.WriteLine("False,It is not a palinrom word");
+            }
+            else
+            {
+                Console.WriteLine("True,It is a palinrom word");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
