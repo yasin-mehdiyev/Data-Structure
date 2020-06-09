92c8999a3503cfe0fd70ea9f7464d38dd4581c0d
yasin-mehdiyev <30857286+yasin-mehdiyev@users.noreply.github.com>
2020-06-09 14:07:58 +0400

    To Find Integer Reversal Number(345-->543,900-->9)
    To Find Integer Reversal Number(345-->543,900-->9)

---
+++ IntegerReversal/IntegerReversal/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IntegerReversal
+{
+    class Program
+    {
+
+        // 900-intReversal 9, 567- intReversal 765
+        // IntegerReversal with Function
+        public static int intReversal(int num)
+        {
+            int remainder = 0;
+            while (num!=0)
+            {
+                int x = num % 10;
+                remainder = remainder * 10 + x;
+                num = (num - x) / 10;
+            }
+            return remainder;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please,Enter any number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            //if (number<0)
+            //{
+            //    number = number * -1;
+            //}
+
+            Console.WriteLine("Reversal Integer: {0}", intReversal(number));
+            Console.ReadKey();
+        }
+    }
+}
